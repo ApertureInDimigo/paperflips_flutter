215578f3a6064ba0938befa4c9d6985fd7a0dc35
Taeyeon <ruadl034@naver.com>
2020-08-04 22:27:27 +0900

    feat : just merge

--- lib/common/asset_path.dart
+++ lib/common/asset_path.dart
@@ -1,7 +1,7 @@
-
 class IconPath {
-  static const String elephant = "images/elephant_icon.png";
-  static const String boat = "images/boat_icon.png";
+  static const String elephant = "/image/elephant";
+  // static const String boat = "images/boat_icon.png";
+  static const String boat = "/image/boat";
   static const String letter_p = "images/p_icon.png";
   static const String fold = "images/fold_icon.png";
   static const String golden_frog = "images/golden_frog_icon.png";

6f3c277d10a3390cbdf8464776878cb05f325e28
woduq1414 <jjy37777@naver.com>
2020-08-10 12:02:39 +0900

    접기 페이지 단계 전환, TTS

--- lib/common/widgets/appbar.dart
+++ lib/common/widgets/appbar.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import '../color.dart';
 import '../font.dart';
 import '../asset_path.dart';
+import 'package:provider/provider.dart';
+import '../../fold_page.dart';
+
 
 class DefaultAppBar extends StatefulWidget implements PreferredSizeWidget {
   DefaultAppBar({this.onActionButtonPressed, this.title});
@@ -43,14 +46,14 @@ class _DefaultAppBarState extends State<DefaultAppBar>
       // Here we take the value from the MyHomePage object that was created by
       // the App.build method, and use it to set our appbar title.
       centerTitle: true,
-      title: _buildAppBarTitle(),
+      title: _buildDefaultAppBarTitle(),
 
       elevation: 0.0,
       flexibleSpace: Container(
         decoration: BoxDecoration(),
       ),
       actions: <Widget>[
-        _buildAppBarActionButton(),
+        _buildDefaultAppBarActionButton(),
         SizedBox(
           width: 30,
         )
@@ -58,7 +61,7 @@ class _DefaultAppBarState extends State<DefaultAppBar>
     );
   }
 
-  Widget _buildAppBarActionButton() {
+  Widget _buildDefaultAppBarActionButton() {
     Widget temp;
     if (onActionButtonPressed != null) {
       temp = IconButton(
@@ -72,7 +75,7 @@ class _DefaultAppBarState extends State<DefaultAppBar>
     return temp;
   }
 
-  Widget _buildAppBarTitle() {
+  Widget _buildDefaultAppBarTitle() {
     Widget temp;
     if (title != null) {
       temp = Text(
@@ -88,26 +91,218 @@ class _DefaultAppBarState extends State<DefaultAppBar>
   }
 }
 
-Widget buildDefaultAppbar() {
-  return AppBar(
-    // Here we take the value from the MyHomePage object that was created by
-    // the App.build method, and use it to set our appbar title.
-    centerTitle: true,
-    title: Container(width: 135, child: Image.asset("images/nav_logo.png")),
+class FoldAppBar extends StatefulWidget implements PreferredSizeWidget {
+  FoldAppBar({this.onPrevButtonPressed,this.onNextButtonPressed, this.title, this.step});
 
-    elevation: 0.0,
-    flexibleSpace: Container(
-      decoration: BoxDecoration(),
-    ),
-    actions: <Widget>[
-      IconButton(
-        icon: new Icon(Icons.person_outline),
-        tooltip: 'Wow',
-        onPressed: () => {},
+  var onPrevButtonPressed;
+  var onNextButtonPressed;
+  var title;
+
+  var step;
+
+  @override
+  _FoldAppBarState createState() =>
+      _FoldAppBarState(onPrevButtonPressed,onNextButtonPressed, title, step);
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
+
+class _FoldAppBarState extends State<FoldAppBar> with TickerProviderStateMixin {
+  String value;
+  var onPrevButtonPressed;
+  var onNextButtonPressed;
+  var title;
+  var step;
+
+  _FoldAppBarState(_onPrevButtonPressed, _onNextButtonPressed, _title, _step) {
+    onPrevButtonPressed = _onPrevButtonPressed;
+    onNextButtonPressed = _onNextButtonPressed;
+    title = _title;
+    step = _step;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    FoldStatus foldStatus = Provider.of<FoldStatus>(context);
+    print(step);
+//    print( Size.fromHeight(kToolbarHeight));
+
+    Widget _buildFoldAppBarNextButton() {
+      return Material(
+        color: Colors.amber,
+        borderRadius: BorderRadius.only(bottomRight: Radius.circular(10)),
+        child: InkWell(
+          splashColor: navColor,
+          onTap: () {foldStatus.nextStep();},
+          borderRadius: BorderRadius.only(bottomRight: Radius.circular(10)),
+          child: Container(
+            height: 56,
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            decoration: BoxDecoration(
+                borderRadius:
+                BorderRadius.only(bottomRight: Radius.circular(10))),
+            child: Row(
+              children: <Widget>[
+                Text(
+                  "다음",
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+                Icon(Icons.arrow_forward),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+
+    void _showExitWarningDialog(){
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          // return object of type Dialog
+          return AlertDialog(
+            title: new Text("진짜 나가요?"),
+            content: new Text("ㄹㅇ?"),
+            actions: <Widget>[
+              RaisedButton(
+                child: new Text("놉"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+               RaisedButton(
+                child: new Text("나가요"),
+                onPressed: () {
+                  Navigator.pop(context); //알림 창 닫힘
+                  Navigator.pop(context); //접기 화면 나가짐
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+
+    Widget _buildFoldAppBarExitButton() {
+      return Material(
+        color: Colors.amber,
+        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10)),
+        child: InkWell(
+          splashColor: navColor,
+          onTap: () {_showExitWarningDialog();},
+          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10)),
+          child: Container(
+            height: 56,
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10))),
+            child: Row(
+              children: <Widget>[
+                Icon(Icons.arrow_back),
+                Text(
+                  "나가기",
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+    
+    
+    Widget _buildFoldAppBarPrevButton() {
+      return Material(
+        color: Colors.amber,
+        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10)),
+        child: InkWell(
+          splashColor: navColor,
+          onTap: () {foldStatus.prevStep();},
+          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10)),
+          child: Container(
+            height: 56,
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10))),
+            child: Row(
+              children: <Widget>[
+                Icon(Icons.arrow_back),
+                Text(
+                  "이전",
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+
+
+    Widget _buildFoldAppBarTitle() {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          foldStatus.getStep() != 1 ? _buildFoldAppBarPrevButton() : _buildFoldAppBarExitButton(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                title,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 14, fontFamily: Font.bold),
+              ),
+              SizedBox(height: 3),
+              Text(
+                "(${foldStatus.getBalance().toString()}/${foldStatus.maxStep})",
+                style: TextStyle(
+                  fontSize: 12,
+                ),
+              )
+            ],
+          ),
+          _buildFoldAppBarNextButton(),
+        ],
+      );
+    }
+
+    return AppBar(
+      // Here we take the value from the MyHomePage object that was created by
+      // the App.build method, and use it to set our appbar title.
+      backgroundColor: primaryColor,
+      centerTitle: true,
+      title: _buildFoldAppBarTitle(),
+
+//      elevation: 0.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          bottom: Radius.circular(10),
+        ),
       ),
-      SizedBox(
-        width: 30,
-      )
-    ],
-  );
+
+      automaticallyImplyLeading: false,
+
+      flexibleSpace: Container(),
+      titleSpacing: 0.0,
+
+//      leading: Row(
+//        children: <Widget>[
+//          SizedBox(width: 20,),
+//          _buildFoldAppBarLeadingButton(),
+//        ],
+//      ),
+    );
+  }
+
+
+
 }
